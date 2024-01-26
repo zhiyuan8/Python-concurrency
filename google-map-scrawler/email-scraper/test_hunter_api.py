1669e75249617b57e7ce151de098689db67e1d3c
Zhiyuan Zack Li <zhiyuanli0718@gmail.com>
2024-01-26 07:00:38 +0000

    init

---
+++ google-map-scrawler/email-scraper/test_hunter_api.py
@@ -0,0 +1,1 @@
+# [hunter API](https://hunter.io/api/email-finder)
--- google-map-scrawler/google-maps-scraper/main.py
+++ google-map-scrawler/google-maps-scraper/main.py
@@ -4,19 +4,20 @@ queries = [
    "bookstore and gift shops near Stanford University",
 ]
 
-fields = [
-   Gmaps.Fields.PLACE_ID, 
-   Gmaps.Fields.NAME,
-   Gmaps.Fields.PHONE, 
-   Gmaps.Fields.ADDRESS,
-   Gmaps.Fields.LINK,
-   Gmaps.Fields.DESCRIPTION,
-   Gmaps.Fields.MAIN_CATEGORY, 
-   # Gmaps.Fields.RATING, 
-   # Gmaps.Fields.REVIEWS, 
-   Gmaps.Fields.WEBSITE, 
-]
+# fields = [
+#    Gmaps.Fields.PLACE_ID, 
+#    Gmaps.Fields.NAME,
+#    Gmaps.Fields.PHONE, 
+#    Gmaps.Fields.ADDRESS,
+#    Gmaps.Fields.LINK,
+#    Gmaps.Fields.DESCRIPTION,
+#    Gmaps.Fields.MAIN_CATEGORY, 
+#    # Gmaps.Fields.RATING, 
+#    # Gmaps.Fields.REVIEWS, 
+#    Gmaps.Fields.WEBSITE, 
+# ]
 
 # You can scrape a maximum of 120 leads per search
-res = Gmaps.places(queries, sort=[Gmaps.  DEFAULT_SORT], fields=fields, has_phone=True, key="AIzaSyA1s9whQHekafdyg9LtcmMZ7tcdl5JxOwM")
-print(res)
+# res = Gmaps.places(queries, sort=[Gmaps.  DEFAULT_SORT], fields=fields, has_phone=True, key="AIzaSyA1s9whQHekafdyg9LtcmMZ7tcdl5JxOwM")
+res = Gmaps.places(queries)
+# print(res)
